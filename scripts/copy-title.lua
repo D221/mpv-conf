be6dc2b6c2be61cd8c48e98d621cd52775f5c1c2
D221 <dailycast@proton.me>
2025-04-16 17:38:23 +0300

    Refactor input and configuration scripts: update key bindings, enhance file moving functionality, and add clipboard path copying feature. Remove obsolete move-file script and introduce autoload script for dynamic playlist management.

--- scripts/copy-title.lua
+++ scripts/copy-title.lua
@@ -18,6 +18,17 @@ function copy_filename_to_clipboard()
     mp.osd_message("Copied filename to clipboard: " .. filename)
 end
 
+-- Define a function to copy the absolute path to the clipboard
+function copy_path_to_clipboard()
+    -- Get the current video absolute path
+    local path = mp.get_property("path")
+    -- Set the clipboard text to the absolute path
+    mp.set_property("clipboard/text", path)
+    -- Notify the user
+    mp.osd_message("Copied path to clipboard: " .. path)
+end
+
 -- Bind the function to Shift+C
 mp.add_key_binding("ctrl+c", "copy-title", copy_title_to_clipboard)
 mp.add_key_binding("Shift+c", "copy-filename", copy_filename_to_clipboard)
+mp.add_key_binding("ctrl+Shift+c", "copy-path", copy_path_to_clipboard)
